07e87de9741857d1a269476e2c982701386a1be4
Kyle Schultz <kyle@kjschultz.com>
2023-10-11 00:08:48 -0700

    rename main script to bot.js and create subcommands for llama slash command. add new channel creation and basic Ollama responses

--- commands/llama/llama.js
+++ commands/llama/llama.js
@@ -1,8 +1,9 @@
 const { log } = require("console");
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 const { Ollama } = require("langchain/llms/ollama");
 const { ChatOllama }  = require("langchain/chat_models/ollama");
 const { StringOutputParser } = require("langchain/schema/output_parser");
+require('dotenv').config();
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,8 +11,16 @@ module.exports = {
         .setDescription('Interact with the Llama.')
         .addSubcommand(subcommand =>
             subcommand
-        		.setName('single')
+        		.setName('q')
                 .setDescription('Single message to Ollama instance - for testing purposes, mostly. No memory.')
+                .addStringOption(option =>
+                    option.setName('message')
+                        .setDescription('Message to send to Ollama.')
+                        .setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('new')
+                .setDescription('Start a new conversation thread with the Llama.')
                 .addStringOption(option =>
                     option.setName('message')
                         .setDescription('Message to send to Ollama.')
@@ -24,8 +33,8 @@ module.exports = {
         const subcommand = interaction.options.getSubcommand();
 
         switch(subcommand) {
-            case 'single':
-                await interaction.deferReply({ ephemeral: true }); // Defer reply to avoid timeouts
+            case 'q':
+                await interaction.deferReply(); // Defer reply to avoid timeouts
                 const message = interaction.options.getString('message'); 
                 const stream = await chat
                     .pipe(new StringOutputParser())
@@ -37,6 +46,16 @@ module.exports = {
                 }
                 reply = chunks.join("");
                 await interaction.editReply(reply);
+                break;
+            case 'new':
+                await interaction.deferReply();
+                newChannel = await interaction.guild.channels.create({
+                    name: "llama-chat",
+                    type: ChannelType.GuildText,
+                    parent: process.env.DISCORD_CATEGORY
+                });
+                console.log(newChannel);
+                await interaction.editReply("Created new channel: " + interaction.guild.channels.cache.get(newChannel.id).toString());
                 break;
             default:
                 await interaction.reply("No subcommand found.");
