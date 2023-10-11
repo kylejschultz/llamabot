b516a37288927ef829cbf6290b077f1eb3db9901
Kyle Schultz <kyle@kjschultz.com>
2023-10-11 12:50:32 -0700

    add read and interaction create events. add util logger

---
+++ events/ready.js
@@ -0,0 +1,20 @@
+const { Events, PermissionFlagsBits, OAuth2Scopes } = require('discord.js');
+const logger = require('../utils/logger');
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    execute(bot) {
+        const link = bot.generateInvite({
+            permissions: [
+                PermissionFlagsBits.Administrator
+            ],
+            scopes: [OAuth2Scopes.Bot],
+        });
+        const Guilds = bot.guilds.cache.map(guild => guild.name);
+        logger.info('============================')
+        logger.info(`Logged in as ${bot.user.tag}!`);
+        logger.info(`Use this invite link to join the server: ${link}`);
+        logger.info('Current Guilds: ' + Guilds.join(', '));
+    },
+};
